f2a99a9cef59a62e18423aa48908c048404cb959
Eric Mertens <emertens@galois.com>
2008-07-31 23:41:13 -0700

    Add BacktrackingUnionFind module

---
+++ BacktrackingUnionFind.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving, MultiParamTypeClasses #-}
+
+module BacktrackingUnionFind (M, runM, unify, testEquiv) where
+
+import MonadLib
+import Control.Monad
+import Control.Monad.ST
+
+import UnionFind
+import RevertArray
+
+newtype M s a = M (StateT (UnionFind RevertArray s) (ChoiceT (ST s)) a)
+  deriving (Monad, MonadPlus)
+
+instance BaseM (ST s) (ST s) where
+    inBase = id
+
+instance BaseM (M s) (ST s) where
+    inBase = M . inBase
+
+-- | Runs the backtracking computation with union/find operations supported
+--   on indexes from 0 to n-1 and returns the first success if one is found.
+runM :: Int -> M s a -> ST s (Maybe a)
+runM n (M m) = do
+    uf <- newUnionFind n
+    findOne $ fmap fst $ runStateT uf m
+
+unify :: Int -> Int -> M s ()
+unify x y = M $ do
+    uf <- get
+    set =<< inBase (union uf x y)
+
+testEquiv :: Int -> Int -> M s Bool
+testEquiv x y = M $ do
+    uf <- get
+    inBase $ liftM2 (==) (find uf x) (find uf y)
